7f7bb1328183b6ab7820849d8e225d10895a6629
sirgraystar <owendavies1993@hotmail.com>
2012-01-30 13:02:18 +0000

    Initial import into Eclipse

--- testsuite/testsuite/internal/CatchExitRule.java
+++ testsuite/testsuite/internal/CatchExitRule.java
@@ -4,27 +4,25 @@ import org.junit.rules.MethodRule;
 import org.junit.runners.model.FrameworkMethod;
 import org.junit.runners.model.Statement;
 
-
 public class CatchExitRule implements MethodRule {
 
-    @Override
     public Statement apply(final Statement base, FrameworkMethod method,
-            Object arg) {
-        return new Statement() {
-            @Override
-            public void evaluate() throws Throwable {
+	    Object arg) {
+	return new Statement() {
+	    @Override
+	    public void evaluate() throws Throwable {
 
-                SecurityManager previous = System.getSecurityManager();
-                CatchExitManager next = new CatchExitManager(previous);
+		SecurityManager previous = System.getSecurityManager();
+		CatchExitManager next = new CatchExitManager(previous);
 
-                try {
-                    System.setSecurityManager(next);
-                    base.evaluate();
-                } finally {
-                    System.setSecurityManager(previous);
-                }
-            }
-        };
+		try {
+		    System.setSecurityManager(next);
+		    base.evaluate();
+		} finally {
+		    System.setSecurityManager(previous);
+		}
+	    }
+	};
     }
 
 }
